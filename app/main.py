eaa499c28b80a9ff1ca6b858fe5d150ff794f288
Vinicius Duzanski <viniciusduzanski@gmail.com>
2025-07-22 22:56:54 -0300

    feat: adding best practices, formatting using blue and isort

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from pipeline.extract import extract_from_excel
-from pipeline.transform import concact_data_frames
 from pipeline.load import load_excel
+from pipeline.transform import concact_data_frames
 
-if __name__ == "__main__":
-    data_frame_list = extract_from_excel("data/input")
+if __name__ == '__main__':
+    data_frame_list = extract_from_excel('data/input')
     data_frame = concact_data_frames(data_frame_list)
-    load_excel(data_frame, "data/output", "output")
+    load_excel(data_frame, 'data/output', 'output')
